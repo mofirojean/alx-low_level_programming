ae0356c0d8660c7c7c5f851f8088fd9ccc92d512
Mofirojean <mofirojean@gmail.com>
2023-08-11 15:12:49 +0100

    2-print_alphabet completed

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: return value of the main function
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
