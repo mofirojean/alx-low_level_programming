362eb22a8536f02989f67e403c6f1cd26e4eca67
Mofirojean <mofirojean@gmail.com>
2023-08-16 21:43:21 +0100

    4-isalpha completed

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check if a character is alphabet
+ * @c: The character to be checked
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
