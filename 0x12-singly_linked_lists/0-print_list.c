5aed5c2bee65db109553c62803369e7e7f499837
Mofirojean <mofirojean@gmail.com>
2023-09-22 02:06:52 +0100

    minor updates

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,43 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t l = 0;
 
 	while (h)
 	{
-	if (h->str == NULL)
-	{
-		_putchar('[');
-		_putchar('0');
-		_putchar(']');
-		_putchar(' ');
-		_putchar('(');
-		_putchar('n');
-		_putchar('i');
-		_putchar('l');
-		_putchar(')');
-		_putchar('\n');
-	}
-	else
-	{
-		size_t len = h->len;
-		const char *str = h->str;
-
-		_putchar('[');
-		_putchar(len + '0');
-		_putchar(']');
-		_putchar(' ');
-
-		while (*str)
-		{
-			_putchar(*str);
-			str++;
-		}
-		_putchar('\n');
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		l++;
 	}
 
-	h = h->next;
-	count++;
-	}
-	return (count);
+	return (l);
 }
