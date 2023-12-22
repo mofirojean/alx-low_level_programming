15b61db3f36fc79053d954770c2698b6a1c80c6b
mofirojean <mofirojean@gmail.com>
2023-12-22 22:54:06 +0100

    fixed set hash error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,3 +1,5 @@
+#include "hash_tables.h"
+
 /**
  * hash_table_set - Add or update an element in a hash table.
  * @ht: A pointer to the hash table.
@@ -11,23 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
 	char *value_copy;
-	unsigned long int indnew = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(value_copy);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-
-	return (1);ex, i;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
